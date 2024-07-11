40b11f66ba750ddb8cdb36e25d77a2eda194c0f0
Gilbert Lagman <gilbertlagmans@gmail.com>
2024-07-11 10:23:20 -0500

    active admin - product page list all product

---
+++ app/admin/products.rb
@@ -0,0 +1,58 @@
+ActiveAdmin.register Product do
+
+  # See permitted parameters documentation:
+  # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
+  #
+  # Uncomment all parameters which should be permitted for assignment
+  #
+  # permit_params :item_description, :stock_quantity, :brand, :category, :sub_category, :color, :storage_capacity, :price, :image_link, :sku
+  #
+  # or
+  #
+  # permit_params do
+  #   permitted = [:item_description, :stock_quantity, :brand, :category, :sub_category, :color, :storage_capacity, :price, :image_link, :sku]
+  #   permitted << :other if params[:action] == 'create' && current_user.admin?
+  #   permitted
+  # end
+
+  permit_params :item_description, :stock_quantity, :brand, :category, :sub_category, :color, :storage_capacity, :price, :image_link
+
+  index do
+    selectable_column
+    id_column
+    column :item_description
+    column :brand
+    column :category
+    column :sub_category
+    column :color
+    column :storage_capacity
+    column :price
+    column :stock_quantity
+    column :image_link
+    actions
+  end
+
+  filter :item_description
+  filter :brand
+  filter :category
+  filter :sub_category
+  filter :color
+  filter :storage_capacity
+  filter :price
+  filter :stock_quantity
+
+  form do |f|
+    f.inputs do
+      f.input :item_description
+      f.input :brand
+      f.input :category
+      f.input :sub_category
+      f.input :color
+      f.input :storage_capacity
+      f.input :price
+      f.input :stock_quantity
+      f.input :image_link
+    end
+    f.actions
+  end
+end
--- app/models/product.rb
+++ app/models/product.rb
@@ -1,8 +1,16 @@
 # app/models/product.rb
-class Product < ApplicationRecord
-  has_many :order_items
-  has_many :cart_items
- # belongs_to :category
 
-  validates :item_description, :stock_quantity, :category, presence: true
+class Product < ApplicationRecord
+  # Associations
+  has_many :cart_items
+  has_many :order_items
+
+  # Ransack configuration
+  def self.ransackable_attributes(auth_object = nil)
+    %w[brand category color created_at id image_link item_description price stock_quantity storage_capacity sub_category updated_at]
+  end
+
+  def self.ransackable_associations(auth_object = nil)
+    %w[cart_items order_items]
+  end
 end
