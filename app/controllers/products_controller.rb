3938e3908a5d154c2f05ae6017828d5818978787
Gilbert Lagman <gilbertlagmans@gmail.com>
2024-07-10 23:40:42 -0500

    listed products - rails server

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,8 +1,12 @@
 class ProductsController < ApplicationController
   def index
+    @products = Product.all
+    puts "Products loaded: #{@products.inspect}" # Debugging line
   end
 
   def show
+    @product = Product.find(params[:id])
+    puts "Product loaded: #{@product.inspect}" # Debugging line
   end
 
   def new
