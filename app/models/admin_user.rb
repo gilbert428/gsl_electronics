fe36f1d45274c953a5d7123dba28b70aa649d4c8
Gilbert Lagman <gilbertlagmans@gmail.com>
2024-07-11 23:29:46 -0500

    working again admin restored

--- app/models/admin_user.rb
+++ app/models/admin_user.rb
@@ -1,11 +1,10 @@
-# app/models/admin_user.rb
-
 class AdminUser < ApplicationRecord
-       # Devise modules
-       devise :database_authenticatable, :recoverable, :rememberable, :trackable, :validatable
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  def self.ransackable_attributes(auth_object = nil)
+    ["created_at", "email", "encrypted_password", "id", "id_value", "remember_created_at", "reset_password_sent_at", "reset_password_token", "updated_at"]
+  end
 
-       # ActiveAdmin-specific configuration
-       def self.ransackable_attributes(auth_object = nil)
-         ["created_at", "email", "encrypted_password", "id", "remember_created_at", "reset_password_sent_at", "reset_password_token", "updated_at"]
-       end
-     end
+  devise :database_authenticatable,
+         :recoverable, :rememberable, :validatable
+end
