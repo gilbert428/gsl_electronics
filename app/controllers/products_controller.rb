2eb95587416bcccaf41f5131881df21d0d899ff9
Gilbert Lagman <gilbertlagmans@gmail.com>
2024-07-11 21:47:11 -0500

    search by category function

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,45 +1,7 @@
 class ProductsController < ApplicationController
+  before_action :set_categories, only: [:index, :show, :search]
+
   def index
-    @categories = Product.select(:category).distinct
-    @products = Product.all
-
-    if params[:category].present?
-      @products = @products.where(category: params[:category])
-    end
-
-    if params[:filter] == "on_sale"
-      @products = @products.where(on_sale: true)
-    elsif params[:filter] == "new"
-      @products = @products.where("created_at >= ?", 3.days.ago)
-    elsif params[:filter] == "recently_updated"
-      @products = @products.where("updated_at >= ?", 3.days.ago)
-    end
-
-    @filtered_products = @products.page(params[:page]).per(10)
-    @all_products = Product.all.page(params[:page]).per(10)
-  end
-
-  def show
-    @product = Product.find(params[:id])
-  end
-
-  def new
-  end
-
-  def create
-  end
-
-  def edit
-  end
-
-  def update
-  end
-
-  def destroy
-  end
-
-  def search
-    @categories = Product.select(:category).distinct
     @products = Product.all
 
     if params[:keyword].present?
@@ -50,8 +12,31 @@ class ProductsController < ApplicationController
       @products = @products.where(category: params[:category])
     end
 
-    @filtered_products = @products.page(params[:page]).per(10)
-    @all_products = Product.all.page(params[:page]).per(10)
+    @products = @products.page(params[:page]).per(10)
+  end
+
+  def show
+    @product = Product.find(params[:id])
+  end
+
+  def search
+    @products = Product.all
+
+    if params[:keyword].present?
+      @products = @products.where("item_description LIKE ?", "%#{params[:keyword]}%")
+    end
+
+    if params[:category].present?
+      @products = @products.where(category: params[:category])
+    end
+
+    @products = @products.page(params[:page]).per(10)
     render :index
+  end
+
+  private
+
+  def set_categories
+    @categories = Product.select(:category).distinct
   end
 end
