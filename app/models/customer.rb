deb2bd05e4d82f48e2a2231fdd1d6f9042cca6ee
Gilbert Lagman <gilbertlagmans@gmail.com>
2024-07-18 14:44:59 -0500

    added cusotmer to active admin

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,15 +1,21 @@
-# app/models/customer.rb
 class Customer < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-
   has_many :orders
   has_many :addresses
   has_many :admins
   has_many :carts
 
-  validates :name, :email, :password_digest, :address, :province, presence: true
+  validates :name, :email, :address, :province, presence: true
+
+  def self.ransackable_attributes(auth_object = nil)
+    ["address", "created_at", "current_sign_in_at", "current_sign_in_ip", "email", "id", "last_sign_in_at", "last_sign_in_ip", "name", "province", "remember_created_at", "reset_password_sent_at", "reset_password_token", "sign_in_count", "updated_at"]
+  end
+
+  def self.ransackable_associations(auth_object = nil)
+    ["orders", "addresses", "admins", "carts"]
+  end
 end
