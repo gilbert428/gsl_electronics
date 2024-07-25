144a94f7b1fd1f1264835f14f42faacf896d1fbc
Gilbert Lagman <gilbertlagmans@gmail.com>
2024-07-25 08:58:37 -0500

    feature_2.4 working

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -35,6 +35,23 @@ class ProductsController < ApplicationController
       products = products.joins(:category).where(categories: { name: params[:category] })
     end
 
+    if params[:category].present?
+      products = products.joins(:category).where(categories: { name: params[:category] })
+    end
+
+    if params[:on_sale].present?
+      products = products.where(sale: true)
+    end
+
+    if params[:new].present?
+      products = products.where('created_at >= ?', 3.days.ago)
+    end
+
+    if params[:recently_updated].present?
+      products = products.where('updated_at >= ?', 3.days.ago).where.not(id: Product.where('created_at >= ?', 3.days.ago).pluck(:id))
+    end
+
+
     products
   end
 end
